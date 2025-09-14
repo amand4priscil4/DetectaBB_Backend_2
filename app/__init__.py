64fdee918e1c76f847e07c6e36d52ef5d5e4f3ac
Amanda Alves <amanda.priscilaa15@gmail.com>
2025-09-14 15:55:08 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -19,7 +19,7 @@ def create_app():
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     
     # Configurar CORS
-   # ✅ Configuração segura
+   # Configuração segura
 CORS(app, 
      resources={r"/*": {"origins": ["http://localhost:3000", "http://localhost:8100", "https://seu-frontend"]}},
      supports_credentials=True,

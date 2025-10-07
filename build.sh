ab1811ce263ca3804f4c2a1f1275be793f696929
Amanda Alves <amanda.priscilaa15@gmail.com>
2025-10-07 21:30:53 +0000

    commit

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 # exit on error
 set -o errexit
 
-# 1. Instala as dependências do sistema (Tesseract)
-apt-get update
-apt-get install -y tesseract-ocr
+# 1. Instala as dependências do sistema com permissão de administrador
+sudo apt-get update
+sudo apt-get install -y tesseract-ocr poppler-utils
 
 # 2. Instala as dependências do Python
 pip install -r requirements.txt
